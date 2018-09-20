9502614b6d6ddde3d745ef4fdc587f2beee27020
poplooukhin@gmail.com <poplooukhin@gmail.com>
2018-09-20 20:41:09 +0300

    Реализовал алгоритм T9.

---
+++ Test/Codec/T9Codec.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Codec
+{
+    public static class T9Codec
+    {
+        private static readonly IDictionary<char, string> SymbolToT9Mapper = new Dictionary<char, string>()
+        {
+            { 'a', "2" },
+            { 'b', "22" },
+            { 'c', "222" },
+            { 'd', "3" },
+            { 'e', "33" },
+            { 'f', "333" },
+            { 'g', "4"},
+            { 'h', "44" },
+            { 'i', "444" },
+            { 'j', "5" },
+            { 'k', "55" },
+            { 'l', "555"},
+            { 'm', "6" },
+            { 'n', "66" },
+            { 'o', "666" },
+            { 'p', "7" },
+            { 'q', "77"},
+            { 'r', "777" },
+            { 's', "7777" },
+            { 't', "8" },
+            { 'u', "88" },
+            { 'v', "888" },
+            { 'w', "9" },
+            { 'x', "99" },
+            { 'y', "999" },
+            { 'z', "9999" },
+            { ' ', "0" }
+        };
+
+        public static string ConvertToT9(this string source)
+        {
+            if (string.IsNullOrWhiteSpace(source))
+                return string.Empty;
+
+            var message = source.Trim().ToLower().Replace('\t', ' ');
+            var regex = new Regex(@"^[a-z]+(\s+[a-z]+)*$");
+            if (!regex.IsMatch(message))
+                throw new ArgumentException(@"Invalid income string. Income string should satisfy the pattern: ^[a-z]+(\s+[a-z]+)*$");
+
+            var encodedSymbols = message.Select(c => SymbolToT9Mapper[c]);
+            var result = string.Join("", encodedSymbols);
+
+            return result;
+        }
+    }
+}
---
+++ Test/Codec/TestT9Codec.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+using NUnit.Framework;
+
+namespace Codec
+{
+    [TestFixture]
+    public class TestT9Codec
+    {
+        [Test]
+        public void ConvertToT9ShouldReturnEmptyResult()
+        {
+            Assert.AreEqual(string.Empty, T9Codec.ConvertToT9(null));
+            Assert.AreEqual(string.Empty, string.Empty.ConvertToT9());
+        }
+
+        [Test]
+        public void ConvertToT9ShouldReturnArgumentException()
+        {
+            Assert.Throws<ArgumentException>(() => "1234567890 hello world".ConvertToT9(),
+                @"Invalid income string. Income string should satisfy the pattern: ^[a-z]+(\s+[a-z]+)*$");
+
+            Assert.Throws<ArgumentException>(() => "hello 1234567890 world".ConvertToT9(),
+                @"Invalid income string. Income string should satisfy the pattern: ^[a-z]+(\s+[a-z]+)*$");
+
+            Assert.Throws<ArgumentException>(() => "hello world 1234567890".ConvertToT9(),
+                @"Invalid income string. Income string should satisfy the pattern: ^[a-z]+(\s+[a-z]+)*$");
+
+            Assert.Throws<ArgumentException>(() => "hello world.".ConvertToT9(),
+                @"Invalid income string. Income string should satisfy the pattern: ^[a-z]+(\s+[a-z]+)*$");
+
+            Assert.Throws<ArgumentException>(() => "hello world!".ConvertToT9(),
+                @"Invalid income string. Income string should satisfy the pattern: ^[a-z]+(\s+[a-z]+)*$");
+
+            Assert.Throws<ArgumentException>(() => "hello world?".ConvertToT9(),
+                @"Invalid income string. Income string should satisfy the pattern: ^[a-z]+(\s+[a-z]+)*$");
+        }
+
+        [Test]
+        public void ConvertToT9ShouldBeOk()
+        {
+            Assert.AreEqual("222222303333304", "AbCd eF G".ConvertToT9());
+            Assert.AreEqual("4444405055555666", "hI j kLmN".ConvertToT9());
+            Assert.AreEqual("66677707777777888088899909999999", "oPq RsTU\tvWx Yz".ConvertToT9());
+        }
+    }
+}
